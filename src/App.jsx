d915b30aead73734527845ca58f07d39d7c3eaf3
matin <matinvsc@gmail.com>
2024-09-30 17:39:26 +0330

    Managing routes in the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,72 @@
-import { BrowserRouter, Routes, Route} from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import GlobalStyled from "./styles/GelobalStyled";
+
+import Dashbord from './pages/Dashboard';
+import Bookings from './pages/Bookings';
+import Cabins from './pages/Cabins';
+import Users from './pages/Users';
+import Settings from './pages/Settings';
+import Account from './pages/Account';
+import Login from './pages/Login';
+import PageNotFound from './pages/PageNotFound';
+import AppLayout from './ui/AppLayout';
+import { Toaster } from 'react-hot-toast';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 60 * 1000,
+    }
+  }
+})
 
 function App() {
 
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
       <GlobalStyled />
       <BrowserRouter>
         <Routes>
+          <Route element={<AppLayout />}>
+            <Route index element={<Navigate replace to="dashbord" />} />
+            <Route path="dashbord" element={<Dashbord />} />
+            <Route path="bookings" element={<Bookings />} />
+            <Route path="cabins" element={<Cabins />} />
+            <Route path="users" element={<Users />} />
+            <Route path="settings" element={<Settings />} />
+            <Route path="account" element={<Account />} />
+          </Route>
 
+          <Route path="login" element={<Login />} />
+          <Route path="*" element={<PageNotFound />} />
         </Routes>
-      </BrowserRouter> 
-    </>
+      </BrowserRouter>
+
+      <Toaster 
+      position='top-center'
+      gutter={12}
+      containerStyle={{margin: '8px'}}
+      toastOptions={{
+        success: {
+          duration: 3000,
+        },
+        error: {
+          duration: 5000,
+        },
+        style: {
+          fontSize: '16px',
+          maxHeight: '500px',
+          padding: '16px 24px',
+          backgroundColor: 'var(--color-grey-0)',
+          color: 'var(--color-grey-700)'
+        }
+      }}
+      />
+      </QueryClientProvider>
   )
 }
 
-export default App
+export default App;
