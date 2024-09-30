d4c77c92c7971ef86b1a571290491255279cde20
matin <matinvsc@gmail.com>
2024-09-30 17:31:11 +0330

    Creating and deleting a new cabin using the react-query and hook-form librarys

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -1,4 +1,10 @@
 import styled from "styled-components";
+import { formatCurrency } from '../../utils/helpers';
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { deleteCabin } from "../../services/apiCabins";
+import toast from "react-hot-toast";
+import { useState } from "react";
+import CreateCabinForm from "./CreateCabinForm";
 
 const TableRow = styled.div`
   display: grid;
@@ -38,3 +44,42 @@ const Discount = styled.div`
   font-weight: 500;
   color: var(--color-green-700);
 `;
+
+
+export default function CabinRow({ cabin }) {
+  const [showForm, setShowForm] = useState(false)
+
+  const { id: cabinId, name, maxCapacity, regularPrice, discount, image } = cabin;
+
+  const queryClient = useQueryClient();
+
+  const { isLoading: isDeleting, mutate } = useMutation({
+    mutationFn: (id) => deleteCabin(id),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ['cabins']
+      })
+      toast.success('cabin successfully deleted')
+    },
+    onError: err => toast.error(err.message)
+  })
+
+  return (
+    <>
+    <TableRow role="row">
+      <Img src={image} />
+      <Cabin>{name}</Cabin>
+      <div>Fits up to {maxCapacity} guests</div>
+      <Price>{formatCurrency(regularPrice)}</Price>
+      <Discount>{formatCurrency(discount)}</Discount>
+      <div>
+        <button onClick={() => setShowForm(show => !show)}>Edit</button>
+        <button disabled={isDeleting} onClick={() => mutate(cabinId)}>
+          Delete
+        </button>
+      </div>
+    </TableRow>
+    {showForm && <CreateCabinForm cabinToEdit={cabin}/>}
+    </>
+  )
+}
