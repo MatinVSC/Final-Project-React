a32db8b74f59f4e122868377ffad3fa96d400166
matin <matinvsc@gmail.com>
2024-10-08 19:24:01 +0330

    updating forms for handling users data

--- src/features/authentication/SignupForm.jsx
+++ src/features/authentication/SignupForm.jsx
@@ -1,35 +1,87 @@
+import { useForm } from "react-hook-form";
+import { useSignup } from "../../hooks/useSignup";
+
 import Button from "../../ui/Button";
 import Form from "../../ui/Form";
 import FormRow from "../../ui/FormRow";
 import Input from "../../ui/Input";
 
-// Email regex: /\S+@\S+\.\S+/
 
 function SignupForm() {
+  const { signup, isLoading } = useSignup();
+  const { register, formState, getValues, handleSubmit, reset } = useForm();
+  const { errors } = formState;
+
+
+  function onSubmit({ fullName, email, password }) {
+    signup({ fullName, email, password }, {
+      onSettled: () => reset()
+    })
+  }
+
   return (
-    <Form>
-      <FormRow label="Full name" error={""}>
-        <Input type="text" id="fullName" />
+    <Form onSubmit={handleSubmit(onSubmit)} >
+      <FormRow label="Full name" error={errors?.fullName?.message}>
+        <Input type="text" id="fullName"
+          {...register('fullName',
+            { require: 'This field is required' }
+          )}
+          disabled={isLoading}
+        />
       </FormRow>
 
-      <FormRow label="Email address" error={""}>
-        <Input type="email" id="email" />
+      <FormRow label="Email address" error={errors?.email?.message}>
+        <Input type="email" id="email"
+          {...register('email',
+            {
+              require: 'This field is required',
+              pattern: {
+                value: /\S+@\S+\.\S+/,
+                message: 'Please provide a valid email address'
+              }
+            }
+          )}
+          disabled={isLoading}
+        />
       </FormRow>
 
-      <FormRow label="Password (min 8 characters)" error={""}>
-        <Input type="password" id="password" />
+      <FormRow label="Password (min 8 characters)" error={errors?.password?.message}>
+        <Input type="password" id="password"
+          {...register('password',
+            {
+              require: 'This field is required',
+              minLength: {
+                value: 8,
+                message: 'Password needs a minimum of 8 Characters'
+              }
+            }
+          )}
+          disabled={isLoading}
+        />
       </FormRow>
 
-      <FormRow label="Repeat password" error={""}>
-        <Input type="password" id="passwordConfirm" />
+      <FormRow label="Repeat password" error={errors?.passwordConfirm?.message}>
+        <Input type="password" id="passwordConfirm"
+          {...register('passwordConfirm',
+            {
+              require: 'This field is required',
+              validate: (value) => value === getValues().password || 'Password need to match'
+            }
+          )}
+          disabled={isLoading}
+        />
       </FormRow>
 
       <FormRow>
         {/* type is an HTML attribute! */}
-        <Button variation="secondary" type="reset">
+        <Button onClick={reset} variation="secondary" type="reset"
+          disabled={isLoading}
+        >
           Cancel
         </Button>
-        <Button>Create new user</Button>
+        <Button disabled={isLoading}>
+          Create new user
+        </Button>
       </FormRow>
     </Form>
   );
--- src/features/authentication/UpdateUserDataForm.jsx
+++ src/features/authentication/UpdateUserDataForm.jsx
@@ -1,12 +1,13 @@
 import { useState } from "react";
+import { useUserLogin } from '../../hooks/useUserLogin';
 
 import Button from "../../ui/Button";
 import FileInput from "../../ui/FileInput";
 import Form from "../../ui/Form";
 import FormRow from "../../ui/FormRow";
 import Input from "../../ui/Input";
+import { useUpdateUser } from "../../hooks/useUpdateUser";
 
-import { useUser } from "./useUser";
 
 function UpdateUserDataForm() {
   // We don't need the loading state, and can immediately use the user data, because we know that it has already been loaded at this point
@@ -15,28 +16,48 @@ function UpdateUserDataForm() {
       email,
       user_metadata: { fullName: currentFullName },
     },
-  } = useUser();
+  } = useUserLogin();
+
+  const { updateUser, isUpdating } = useUpdateUser();
 
   const [fullName, setFullName] = useState(currentFullName);
   const [avatar, setAvatar] = useState(null);
 
   function handleSubmit(e) {
     e.preventDefault();
+    if (!fullName) return;
+
+    updateUser({ fullName, avatar },
+      {
+        onSuccess: () => {
+          setAvatar(null);
+          e.target.reset();
+        }
+      });
+  };
+
+  function handelCancel() {
+    setFullName(currentFullName);
+    setAvatar(null);
   }
 
   return (
     <Form onSubmit={handleSubmit}>
       <FormRow label="Email address">
-        <Input value={email} disabled />
+        <Input value={email} disabled={isUpdating}
+        />
       </FormRow>
+
       <FormRow label="Full name">
         <Input
           type="text"
           value={fullName}
           onChange={(e) => setFullName(e.target.value)}
           id="fullName"
+          disabled={isUpdating}
         />
       </FormRow>
+
       <FormRow label="Avatar image">
         <FileInput
           id="avatar"
@@ -44,11 +65,13 @@ function UpdateUserDataForm() {
           onChange={(e) => setAvatar(e.target.files[0])}
         />
       </FormRow>
+
       <FormRow>
-        <Button type="reset" variation="secondary">
+        <Button onClick={handelCancel} type="reset" variation="secondary"
+          disabled={isUpdating}>
           Cancel
         </Button>
-        <Button>Update account</Button>
+        <Button disabled={isUpdating}>Update account</Button>
       </FormRow>
     </Form>
   );
